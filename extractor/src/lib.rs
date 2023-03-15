e519fc93f2aded56879b5aab3ddf6ce23c137228
nayan <nayansharma1010@gmail.com>
2023-03-15 12:35:24 +0530

    feat: #1 meta data extraction

--- extractor/src/lib.rs
+++ extractor/src/lib.rs
@@ -1,44 +1,81 @@
 use gstreamer as gst;
+use gstreamer_pbutils as gst_pbutils;
 use gst::prelude::*;
+use std::env;
 
-pub fn extract() {
-    // Initialize GStreamer
-    gst::init().unwrap();
+use anyhow::Error;
+use derive_more::{Display, Error};
+use gst_pbutils::{prelude::*, DiscovererInfo, DiscovererStreamInfo};
+#[derive(Debug, Display, Error)]
+#[display(fmt = "Discoverer error {_0}")]
+struct DiscovererError(#[error(not(source))] &'static str);
 
-    // Build the pipeline
-    let uri =
-        "https://ia804505.us.archive.org/20/items/MIT6.006S20/MIT6_006S20_02_04_Lecture_1_300k.mp4";
-    let pipeline = gst::parse_launch(&format!("playbin uri={uri}")).unwrap();
-    
-    // Start playing
-    pipeline
-        .set_state(gst::State::Playing)
-        .expect("Unable to set the pipeline to the `Playing` state");
+fn print_tags(info: &DiscovererInfo) {
+    println!("Tags:");
 
-       
-
-    // Wait until error or EOS
-    let bus = pipeline.bus().unwrap();
-    for msg in bus.iter_timed(gst::ClockTime::NONE) {
-        use gst::MessageView;
-
-        match msg.view() {
-            MessageView::Eos(..) => break,
-            MessageView::Error(err) => {
-                println!(
-                    "Error from {:?}: {} ({:?})",
-                    err.src().map(|s| s.path_string()),
-                    err.error(),
-                    err.debug()
-                );
-                break;
+    let tags = info.tags();
+    match tags {
+        Some(taglist) => {
+            println!("  Tags:");
+            for tag in taglist.iter() {
+                println!("{:?}", tag);
             }
-            _ => (),
+        }
+        None => {
+            println!("  no tags");
         }
     }
-
-    // Shutdown pipeline
-    pipeline
-        .set_state(gst::State::Null)
-        .expect("Unable to set the pipeline to the `Null` state");
 }
+
+fn print_stream_info(stream: &DiscovererStreamInfo) {
+    println!("Stream: ");
+    println!("  Stream id: {}", stream.stream_id());
+    let caps_str = match stream.caps() {
+        Some(caps) => caps.to_string(),
+        None => String::from("--"),
+    };
+    println!("  Format: {caps_str}");
+}
+
+fn print_discoverer_info(info: &DiscovererInfo) -> Result<(), Error> {
+    println!("URI: {}", info.uri());
+    println!("Duration: {}", info.duration().display());
+    print_tags(info);
+    print_stream_info(
+        &info
+            .stream_info()
+            .ok_or(DiscovererError("Error while obtaining stream info"))?,
+    );
+
+    let children = info.stream_list();
+    println!("Children streams:");
+    for child in children {
+        print_stream_info(&child);
+    }
+
+    Ok(())
+}
+
+fn run_discoverer() -> Result<(), Error> {
+    gst::init()?;
+    let args: Vec<_> = env::args().collect();
+    let uri: &str = if args.len() == 2 {
+        args[1].as_ref()
+    } else {
+        println!("Usage: discoverer uri");
+        std::process::exit(-1)
+    };
+    let timeout: gst::ClockTime = gst::ClockTime::from_seconds(15);
+    let discoverer = gst_pbutils::Discoverer::new(timeout)?;
+    let info = discoverer.discover_uri(uri)?;
+    print_discoverer_info(&info)?;
+    Ok(())
+}
+
+pub fn example_main() {
+    match run_discoverer() {
+        Ok(_) => (),
+        Err(e) => eprintln!("Error: {e}"),
+    }
+}
+
--- helper/src/main.rs
+++ helper/src/main.rs
@@ -1,4 +1,4 @@
 use extractor;
 fn main() {
-   extractor::extract();
+   extractor::example_main();
 }
