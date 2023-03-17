e1ef3ec6c7c9ed620609ac8ecf6c4a78b232abde
nayan <nayansharma1010@gmail.com>
2023-03-17 11:13:20 +0530

    reactor: #1 hash value extraction

--- backend/src/meta_data/services.rs
+++ backend/src/meta_data/services.rs
@@ -10,135 +10,33 @@ use crate::AppState;
 async fn post_meta_data(pool_data: web::Data<AppState>,param_obj: web::Json<VideoUri>) -> impl Responder {
     let pool = &pool_data.pool;
 
-    let mut hash_tag: HashMap<String,String> = HashMap::new(); 
     let tags = extract_data(&param_obj.uri).await;
-    for tag in tags.iter() {
-        hash_tag.insert(tag.meta_title.clone(), tag.meta_value.clone());  
-    }
-    let mut max_bitrate: u32 = 0;
-    let mut bitrate: u32 = 0;
-    let mut uri: String = String::new();
-    let mut audio_codec: String = String::new();
-    let mut language_code: String = String::new();
-    let mut title: String = String::new();
-    let mut artist: String = String::new();
-    let mut album_artist: String = String::new();
-    let mut album: String = String::new();
-    let mut copyright: String = String::new();
-    let mut comment: String = String::new();
-    let mut description: String = String::new();
-    let mut date: String = String::new();
-    let mut encoder: String = String::new();
-    let mut genre: String = String::new();
-    let mut image: String = String::new();
-    let mut keywords: String = String::new();
-    let mut private_qt_tag: String = String::new();
-    let mut container_format: String = String::new();
-    let mut video_codec: String = String::new();
-    let mut duration: String = String::new();
-   
 
-    if let Some(u) = hash_tag.get("uri") {
-        uri.push_str(u);
-    }else {
-        uri.push_str("NULL");
-    }
-    if let Some(u) = hash_tag.get("audio-codec") {
-        audio_codec.push_str(u);
-    }else {
-        audio_codec.push_str("NULL");
-    }
-    if let Some(u) = hash_tag.get("maximum-bitrate") {
-        max_bitrate = u.parse::<u32>().unwrap();
-    }
-    if let Some(u) = hash_tag.get("bitrate") {
-        bitrate = u.parse::<u32>().unwrap();
-    }
-    if let Some(u) = hash_tag.get("language-code") {
-        language_code.push_str(u);
-    }else {
-        language_code.push_str("NULL");
-    }
-    if let Some(u) = hash_tag.get("title") {
-        title.push_str(u);
-    }else {
-        title.push_str("NULL");
-    }
-    if let Some(u) = hash_tag.get("artist") {
-        artist.push_str(u);
-    }else {
-        artist.push_str("NULL");
-    }
-    if let Some(u) = hash_tag.get("album-artist") {
-        album_artist.push_str(u);
-    }else {
-        album_artist.push_str("NULL");
-    }
-    if let Some(u) = hash_tag.get("album") {
-        album.push_str(u);
-    }else {
-        album.push_str("NULL");
-    }
-    if let Some(u) = hash_tag.get("copyright") {
-        copyright.push_str(u);
-    }else {
-        copyright.push_str("NULL");
-    }
-    if let Some(u) = hash_tag.get("comment") {
-        comment.push_str(u);
-    }else {
-        comment.push_str("NULL");
-    }
-    if let Some(u) = hash_tag.get("description") {
-        description.push_str(u);
-    }else {
-        description.push_str("NULL");
-    }
-    if let Some(u) = hash_tag.get("date") {
-        date.push_str(u);
-    }else {
-        date.push_str("1979-01-01");
-    }
-    if let Some(u) = hash_tag.get("encoder") {
-        encoder.push_str(u);
-    }else {
-        encoder.push_str("NULL");
-    }
-    if let Some(u) = hash_tag.get("genre") {
-        genre.push_str(u);
-    }else {
-        genre.push_str("NULL");
-    }
-    if let Some(u) = hash_tag.get("image") {
-        image.push_str(u);
-    }else {
-        image.push_str("NULL");
-    }
-    if let Some(u) = hash_tag.get("keywords") {
-        keywords.push_str(u);
-    }else {
-        keywords.push_str("NULL");
-    }
-    if let Some(u) = hash_tag.get("private-qt-tag") {
-        private_qt_tag.push_str(u);
-    }else {
-        private_qt_tag.push_str("NULL");
-    }
-    if let Some(u) = hash_tag.get("container-format") {
-        container_format.push_str(u);
-    }else {
-        container_format.push_str("NULL");
-    }
-    if let Some(u) = hash_tag.get("video-codec") {
-        video_codec.push_str(u);
-    }else {
-        video_codec.push_str("NULL");
-    }
-    if let Some(u) = hash_tag.get("duration") {
-        duration.push_str(u);
-    }else {
-        duration.push_str("NULL");
-    }
+    let hash_tag: HashMap<String,String> = tags.iter().map(|tag| (tag.meta_title.clone(),tag.meta_value.clone())).collect();
+    let null_default: String = "NULL".to_string();
+    let date_default: String = "1979-01-01".to_string();
+
+    let uri = hash_tag.get("uri").unwrap_or(&null_default);
+    let audio_codec = hash_tag.get("audio-codec").unwrap_or(&null_default);
+    let max_bitrate = hash_tag.get("maximum-bitrate").and_then(|v| v.parse().ok()).unwrap_or(0);
+    let bitrate = hash_tag.get("bitrate").and_then(|v| v.parse().ok()).unwrap_or(0);
+    let language_code = hash_tag.get("language-code").unwrap_or(&null_default);
+    let title = hash_tag.get("title").unwrap_or(&null_default);
+    let artist = hash_tag.get("artist").unwrap_or(&null_default);
+    let album_artist = hash_tag.get("album-artist").unwrap_or(&null_default);
+    let album = hash_tag.get("album").unwrap_or(&null_default);
+    let copyright = hash_tag.get("copyright").unwrap_or(&null_default);
+    let comment = hash_tag.get("comment").unwrap_or(&null_default);
+    let description = hash_tag.get("description").unwrap_or(&null_default);
+    let date = hash_tag.get("date").unwrap_or(&date_default);
+    let encoder = hash_tag.get("encoder").unwrap_or(&null_default);
+    let genre = hash_tag.get("genre").unwrap_or(&null_default);
+    let image = hash_tag.get("image").unwrap_or(&null_default);
+    let keywords = hash_tag.get("keywords").unwrap_or(&null_default);
+    let private_qt_tag = hash_tag.get("private-qt-tag").unwrap_or(&null_default);
+    let container_format = hash_tag.get("container-format").unwrap_or(&null_default);
+    let video_codec = hash_tag.get("video-codec").unwrap_or(&null_default);
+    let duration = hash_tag.get("duration").unwrap_or(&null_default);
 
     sqlx::query("INSERT INTO media_files (
         uri, audio_codec, max_bitrate, bitrate, language_code, title, artist,
@@ -146,11 +44,7 @@ async fn post_meta_data(pool_data: web::Data<AppState>,param_obj: web::Json<Vide
         date, encoder, genre, image, keywords, private_qt_tag, container_format,
         video_codec,duration
     ) VALUES (
-            ?, ?, ?, ?, ?, ?,
-            ?, ?, ?, ?, ?,
-            ?,
-            ?, ?,?,?,
-            ?, ?, ?, ?,?
+            ?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?
         )")
         .bind(uri)
         .bind(audio_codec)
